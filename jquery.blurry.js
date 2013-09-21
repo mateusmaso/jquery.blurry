d39865a66527091c7cb3adc221c004c9a86c3f9f
Mateus Maso <m.maso25@gmail.com>
2013-09-21 15:16:24 -0700

    Created jquery.blurry.js

---
+++ jquery.blurry.js
@@ -0,0 +1,26 @@
+(function($) {
+
+  $.fn.blurry = function(strength) {
+    var image = $(this)[0];
+    var element = $('<canvas/>')[0];
+    element.width = image.width;
+    element.height = image.height;
+    
+    var context = element.getContext("2d");
+    context.drawImage(image, 0, 0, image.width, image.height);
+    context.globalAlpha = 0.5; 
+
+    for (var y = -strength; y <= strength; y += 2) {
+      for (var x = -strength; x <= strength; x += 2) {
+        context.drawImage(element, x, y);
+        if (x>=0 && y>=0) {
+          context.drawImage(element, -(x-1), -(y-1));
+        }
+      }
+    }
+    
+    context.globalAlpha = 1.0;
+    $(this).replaceWith(element);
+  };
+
+})(jQuery);
